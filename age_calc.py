8907485ae2a099cc8ee2de28b37d54afdb57ec80
Taylor Fritz <tayfritz01@gmail.com>
2020-09-17 12:09:17 -0400

    Age exercise

---
+++ age_calc.py
@@ -0,0 +1,8 @@
+# Step 1
+# Ask the user for their name and the year they were born.
+
+# Step 2
+# Calculate and print the year they'll turn 25, 50, 75, and 100.
+
+# Step 3
+# If they're already past any of these ages, skip them.
