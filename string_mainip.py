8d2e7187ca80954c6e987a6814ae453436a26269
Taylor Fritz <tayfritz01@gmail.com>
2020-09-17 14:30:38 -0400

    Working with strings and lists exercise

---
+++ string_mainip.py
@@ -0,0 +1,17 @@
+# Step 1:
+# Make two strings, each should be 8 characters long, made up of Xs and Os.
+# First string should start with X, second string should start with O.
+# Both strings should alternate between the two characters.
+# You can use multiplication for this.
+
+
+# Step 2:
+# Make a list
+# Add 1 of the X-started strings.
+# Add 1 of the O-started strings.
+# Repeat until you have 8 items total in the list.
+# You can use multiplication for this, too.
+
+
+# Step 3:
+# Print out the list of strings, joined with newlines \n.
