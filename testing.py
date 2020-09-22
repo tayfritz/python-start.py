aa20691f38608816b9325956fe41360e4bfc0825
Taylor Fritz <tayfritz01@gmail.com>
2020-09-22 16:08:22 -0400

    new exercises added to practice sequence operations

---
+++ testing.py
@@ -0,0 +1,1 @@
+docs = 'Tuples are immutable sequences, typically used to store collections of heterogeneous data (such as the 2-tuples produced by the enumerate() built-in). Tuples are also used for cases where an immutable sequence of homogeneous data is needed (such as allowing storage in a set or dict instance).'
