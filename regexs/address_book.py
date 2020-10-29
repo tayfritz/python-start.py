6a997599fad3544b045b55eab6358505f04eb689
Taylor Fritz <tayfritz01@gmail.com>
2020-10-29 19:16:14 -0400

    regex practice and raw strings intro

--- regexs/address_book.py
+++ regexs/address_book.py
@@ -1,4 +1,11 @@
+import re
+
 names_file = open("names.txt", encoding='utf-8')
 data = names_file.read()
 names_file.close()
 
+
+last_name = r'Love'
+first_name = r'Kenneth'
+print(re.match(last_name, data))
+print(re.search(first_name, data))
